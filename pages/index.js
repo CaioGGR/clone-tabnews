4bfd2cc768d8162037d478fb506020fcf7d9f234
Caio Ribeiro <caioggr@gmail.com>
2023-12-05 00:56:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Eai gatinha, vamos fazer uma saliência? 🥵</h1>
+  return <h1>Eai gatinha, vamos fazer uma saliência? 🥵</h1>;
 }
 
-export default Home
+export default Home;
