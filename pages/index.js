d570a88e3bc5ef72c5190d313ab31b2fa7f91374
Caio Ribeiro <caioggr@gmail.com>
2023-12-17 23:45:44 +0000

    alterando a página inicial com aviso de `em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 function Home() {
-  return (
-    <h1>Esse site está em construção, logo mais você poderá usufruir dele.</h1>
+  const homePage = (
+    <div>
+      <p align="center"></p>
+      <title>SeraQueVocêDev?</title>
+      <h1 align="center">Site em construção! ⚒️</h1>
+
+      <p align="center">
+        Em breve um novo site com uma proposta um tanto diferente de animar DEVs
+        em formação. 🖥️
+      </p>
+    </div>
   );
+  return homePage;
 }
 
 export default Home;
